b303c912c86179ce8283172e7a73832a96a94243
murgraydj <murgraydj@gmail.com>
2022-10-05 16:18:20 -0400

    Adds basic calculating functions in js
    Just the main 4: add, subtract, multiply, and divide.

---
+++ script.js
@@ -0,0 +1,20 @@
+//Author: Murgray D. John
+//Date: Started 10/5/2022
+//Purpose: To create a functional calculator
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
