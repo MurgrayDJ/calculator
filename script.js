65895c6cbe1d4354e78b49265769533a130b7ded
murgraydj <murgraydj@gmail.com>
2022-10-05 21:15:35 -0400

    Single digit calculations work
    If a single digit calculation is performed (a + b = c) the correct
    answer will show on the display. This includes calculations involving 0.
    Multi-step calculations (a + b + c = d) do not work. Multi-digit
    calculations do not work.

--- script.js
+++ script.js
@@ -40,6 +40,8 @@ const buttons = document.querySelectorAll('button');
     })
 });
 
+let firstValue, secondValue, operator, currentTotal;
+
 const display = document.getElementById('display');
 function changeDisplay(btnSymbol){
     let currentValue = parseInt(display.value);
@@ -47,17 +49,42 @@ function changeDisplay(btnSymbol){
     
     //If current value is a number and button click is a number
     if(!isNaN(currentValue) && !isNaN(nextSymbol)){
-        if(currentValue === 0){
-            display.value = '';
+        //Restart
+        if(currentValue === 0 || (currentValue !== 0 && currentTotal)){
+            display.value = nextSymbol;
+            firstValue = parseInt(display.value);
+            secondValue, operator, currentTotal = null;
         }
-        display.value = display.value + btnSymbol;
+        // else if(currentValue !== 0 && currentTotal){
+
+        // }
+        else{
+            display.value = display.value + btnSymbol;
+            if(secondValue){
+                secondValue = parseInt(display.value);
+            }
+        }
+        
+        //firstValue = parseInt(display.value);
     }
     //If current value is a number and button click is not a number
     else if(!isNaN(currentValue) && isNaN(nextSymbol)){
+        if(!firstValue && firstValue !== 0){
+            firstValue = parseInt(display.value);
+        }
         display.value = btnSymbol;
+        
+        if(btnSymbol === "="){
+            display.value = operate(operator, firstValue, secondValue);
+            currentTotal = parseInt(display.value);
+        }
+        //operator = btnSymbol;
     }
     //If current value is not a number and button click is a number
     else if(isNaN(currentValue) && !isNaN(nextSymbol)){
+        operator = display.value;
         display.value = btnSymbol;
+        secondValue = parseInt(display.value);
+        //display.value = operate(operator, firstValue, secondValue);
     }
 }
